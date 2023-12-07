61474e22ce5e5ea9bb24679fe55821183c249b2c
sohyeon Park <qkrthgus7026.gmail.com>
2023-12-07 17:17:40 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kumoh/ce/s20190467/s23w1301songlist/SongViewModel.kt
@@ -0,0 +1,42 @@
+package kr.ac.kumoh.ce.s20190467.s23w1301songlist
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class SongViewModel : ViewModel() {
+    companion object{
+        const val SERVER_URL = "https://port-0-s23w10backend-1igmo82clookyw7l.sel5.cloudtype.app/"
+    }
+
+    private val _songList = MutableLiveData<List<Song>>()
+    val songList: LiveData<List<Song>>
+        get() = _songList
+
+    private val songApi : SongApi
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(SERVER_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        songApi = retrofit.create(SongApi::class.java)
+        fetchSong()
+    }
+
+    private fun fetchSong(){
+        viewModelScope.launch {
+            try {
+                val response = songApi.getSongs()
+                _songList.value = response
+            } catch (e: Exception) {
+                Log.e("fetchData()", e.toString())
+            }
+        }
+    }
+}
